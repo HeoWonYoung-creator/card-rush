a20a791135af717ceea7c9f90431336484388946
HeoWonYoung-creator <tale6314sc@gmail.com>
2025-08-10 01:32:31 +0900

    Delete functions directory

--- functions/api/health.js
+++
@@ -1,6 +0,0 @@
-export const onRequestGet = () =>
-  new Response(JSON.stringify({ ok: true, message: 'Server is healthy' }), {
-    headers: { 'content-type': 'application/json; charset=utf-8' },
-  });
-
-
